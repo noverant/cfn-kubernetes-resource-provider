1bad06bd1f86f151307221374417fac61548ae55
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2023-03-07 11:32:45 -0600

    Updating handlers.py description

--- src/awsqs_kubernetes_resource/handlers.py
+++ src/awsqs_kubernetes_resource/handlers.py
@@ -1,5 +1,5 @@
 def print_handler():
-    print('Kubernetyes Resource Types for AWS CloudFormation\n\nThis project has been deprecated, and is no '
+    print('Kubernetes Resource Types for AWS CloudFormation\n\nThis project has been deprecated, and is no '
           'longer supported. You are free to fork the code in the main branch and use it as a private resource '
           'type, but no support will be given by the AWS I&A or Cloudformation Teams.\n\n**USE AT YOUR OWN RISK**')
 
