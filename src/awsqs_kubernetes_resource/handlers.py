4d62936ebdad974adb044e960f6f2056bdc1db34
Troy Ameigh <55170274+troy-ameigh@users.noreply.github.com>
2023-04-24 14:47:25 -0500

    updated for retirement

--- src/awsqs_kubernetes_resource/handlers.py
+++
@@ -1,5 +0,0 @@
-def print_handler():
-    print('Kubernetes Resource Types for AWS CloudFormation\n\nThis project has been retired, and will no longer be supported or maintained after March 31, 2023. '
-          'You are free to fork the code in the main branch and use it as a private resource type.\n\n**USE AT YOUR OWN RISK**')
-
-print_handler()
